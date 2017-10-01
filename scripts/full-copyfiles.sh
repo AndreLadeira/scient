0f2d14b505875890a75b2471ec2463f090a14742
ladeira@asimov <andreladeira.dev@gmail.com>
2017-10-01 17:04:22 -0300

    new file:   .gitignore 	deleted:    index-old.html 	deleted:    layouts-offcanvas-navigation.html 	modified:   scripts/full-copyfiles.sh

--- scripts/full-copyfiles.sh
+++ scripts/full-copyfiles.sh
@@ -1,26 +1,16 @@
-DIR="/var/www/codeworks.ddns.net/public_html"
+DIR="/var/www/scient.ddns.net/public_html"
 
 rm -rf $DIR/*
 
-mkdir "$DIR/authors"
-mkdir "$DIR/content"
 mkdir "$DIR/css"
-mkdir "$DIR/data"
-mkdir "$DIR/font"
-mkdir "$DIR/html"
+mkdir "$DIR/fonts"
 mkdir "$DIR/img"
 mkdir "$DIR/js"
 mkdir "$DIR/php"
 
 
-cp -f -R ../authors/* "$DIR/authors"
-cp -f -R ../content/* "$DIR/content"
 cp -f ../css/* "$DIR/css"
-cp -f ../data/* "$DIR/data"
-cp -f ../font/* "$DIR/font"
-cp -f ../html/* "$DIR/html"
+cp -f ../font/* "$DIR/fonts"
 cp -f ../img/* "$DIR/img"
 cp -R ../js/*  "$DIR/js"
-cp -f ../php/* "$DIR/php"
-cp -f -R ../posts/* "$DIR/posts"
-cp -f ../* "$DIR"
+cp -f ../*.html "$DIR"
